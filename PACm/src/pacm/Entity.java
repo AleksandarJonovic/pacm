5b02371bc7bfafa55fcec2ef94cb6773134a4f4c
AleksandarJonovic <jonostranz@gmail.com>
2014-02-18 16:20:24 +0100

    entity skabelon
    first things first

---
+++ PACm/src/pacm/Entity.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacm;
+
+/**
+ *
+ * @author Alex
+ */
+public abstract class Entity {
+    
+    private int xPos;
+    private int yPos;
+    private boolean dead;
+    
+    public void move(int placeholder){}
+    
+    
+}
